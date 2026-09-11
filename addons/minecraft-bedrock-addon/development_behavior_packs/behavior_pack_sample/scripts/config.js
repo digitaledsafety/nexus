ff8d2653d664ad734596104818c9ad9aa10416df
brettfxio <105930054+brettfxio@users.noreply.github.com>
2026-09-11 10:59:25 -0600

    Update config.js

--- addons/minecraft-bedrock-addon/development_behavior_packs/behavior_pack_sample/scripts/config.js
+++ addons/minecraft-bedrock-addon/development_behavior_packs/behavior_pack_sample/scripts/config.js
@@ -1,3 +1,3 @@
 export const WS_URL = "ws://127.0.0.1:9001";
-export const SERVER_ID = "local-dev";
+export const SERVER_ID = "minecraft-server-1";
 export const NEXUS_ADDRESS = "0x0000000000000000000000000000000000000000";
